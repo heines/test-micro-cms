d391a72188bbc58ef5581adb9244f91a57d24e1e
shimizu s <s-shimizu@tsumikiinc.com>
2022-08-09 12:32:55 +0900

    Revert "Revert "#2 デザイン修正、統一化""

--- pages/search/_keyword.vue
+++ pages/search/_keyword.vue
@@ -1,22 +1,24 @@
 <template lang="pug">
-  .tag
-    h1.tag__title
+.content
+  .content__header
+    h1.content__title
       |キーワード: {{ $route.params.keyword }}
-    div
-      b-card-group(
-        card-deck
+  .content__body
+    b-card-group(
+      card-deck
+      )
+      b-link(
+        v-for="(item, index) in items"
+        :to="{name: 'id', params: {id: item.id}}"
+        :key="item.id"
+        class="p-3"
         )
-        b-link(
-          v-for="(item, index) in items"
-          :to="{name: 'id', params: {id: item.id}}"
-          :key="item.id"
-          class="p-3"
+        b-card(
+          :title="item.title"
           )
-          b-card(
-            :title="item.title"
-            )
-            b-card-text
-              |{{ $dateFns.format(new Date(item.createdAt), 'dd/MM/yyyy') }}
+          b-card-text
+            |{{ $dateFns.format(new Date(item.createdAt), 'dd/MM/yyyy') }}
+  .content__footer
     b-button(
       to = "/"
       variant="outline-primary"
@@ -44,17 +46,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-.tag {
-  @media (min-width: 992px) {
-    padding: 5em;
-  }
-  @media (max-width: 991px) {
-    padding: 2em;
-  }
-  &__time {
-    text-align: right;
-  }
-}
-</style>
